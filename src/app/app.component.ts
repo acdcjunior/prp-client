1bc71e31e3b57c76e4640943aee863286376fa44
acdcjunior <acdcjunior@gmail.com>
2016-07-12 22:57:37 -0300

    provide CookieService pra toda a aplicacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES }  from '@angular/router';
+import {CookieService} from "angular2-cookie/core";
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
+  providers: [CookieService],
   directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent {
